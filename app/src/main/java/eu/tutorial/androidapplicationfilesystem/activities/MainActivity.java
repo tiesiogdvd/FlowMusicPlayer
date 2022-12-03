edeb596346ef463e8afad0111fa18f8044884489
tiesiogdvd <deivukelis@gmail.com>
2022-12-03 17:26:26 +0200

    Implemented dialog popup for playlists, moves parts of the code out to make this activity less clutered. Created metadata retrievers and setters.

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/activities/MainActivity.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package eu.tutorial.androidapplicationfilesystem;
+package eu.tutorial.androidapplicationfilesystem.activities;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -8,16 +8,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.Manifest;
-import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.io.File;
+
+import eu.tutorial.androidapplicationfilesystem.R;
+import eu.tutorial.androidapplicationfilesystem.classes.MetadataGetterSetter;
+import eu.tutorial.androidapplicationfilesystem.classes.MusicData;
 import soup.neumorphism.NeumorphButton;
 import soup.neumorphism.NeumorphImageButton;
 
@@ -32,21 +45,50 @@ public class MainActivity extends AppCompatActivity {
     NeumorphImageButton btnMusicImage;
     MediaPlayer mp;
     String receivedPath;
+    MediaMetadataRetriever metadataRetriever;
+    TextView songName;
+    TextView artistName;
+    SeekBar seekBar;
+    MusicData musicData;
+    File[] filesAndFolders;
+    File musicFile;
+    MetadataGetterSetter metadataGetterSetter;
 
+    private void initiateMethods(){
+        btnStorage = findViewById(R.id.btnStorage);
+        btnPlay = findViewById(R.id.playButton);
+        btnPrevious = findViewById(R.id.lastButton);
+        btnNext = findViewById(R.id.nextButton);
+        btnPlaylist = findViewById(R.id.playlistButton);
+        btnFavorite = findViewById(R.id.favoriteButton);
+        btnMusicImage = findViewById(R.id.musicImage);
+        metadataRetriever = new MediaMetadataRetriever();
+        songName = findViewById(R.id.musicSongName);
+        artistName = findViewById(R.id.musicSongArtist);
+        seekBar = findViewById(R.id.musicProgress);
 
+    }
+
+    //ActivityResultLauncher waits for the FileListActivity to finish and retrieves the data provided from that activity
     ActivityResultLauncher <Intent> activityLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 public void onActivityResult(ActivityResult result) {
+
                     if(result.getResultCode()==1){
+                        //ResultCode 1 means that music data has been received amd executes this part of code
                         Intent intent = result.getData();
-                        if(intent!=null){
-                            String data = intent.getStringExtra("result");
-                            receivedPath = data;
-                            System.out.println(data);
-                            play(data);
-                            Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+                        if(intent!=null){ //confirms that there is actual music data provided in the result
+                            receivedPath = intent.getStringExtra("result");
+
+                            MetadataGetterSetter metadataGetterSetter = new MetadataGetterSetter();
+                            //MetadataGetterSetter class is called to receive metadata with the help of MusicData class and then put it into MainActivity Views
+                            metadataGetterSetter.retrieveMetadata(receivedPath, MainActivity.this);
+                            //For it to work it is required the audio file path and MainActivity context
+
+                            play(receivedPath);
                         }
+
                     }
                 }
             }
@@ -64,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
         mp.start();
         btnPlay.setImageResource(R.drawable.ic_action_pause);
     }
-
     private void Pause(){
         mp.pause();
         btnPlay.setImageResource(R.drawable.ic_action_play);
@@ -75,33 +116,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);  //to hide top navbar
-
         getSupportActionBar().hide(); //to hide top Application Bar
         setContentView(R.layout.activity_main);
-
-        mp = MediaPlayer.create(this, R.raw.music);
+        initiateMethods();
         setOnClickListeners();
+        mp = MediaPlayer.create(this, R.raw.music);
 
+        String path1 = Environment.getExternalStorageDirectory().getPath();
+        path1+="/MuzikaTest/Rammstein - Rammstein (2019) [320]/05 SEX.mp3";
+        File testFile = new File(path1);
+        mp = MediaPlayer.create(this, Uri.parse(testFile.getAbsolutePath()));
 
 
     }
 
     private void setOnClickListeners(){
-
-        btnStorage = findViewById(R.id.btnStorage);
-        btnPlay = findViewById(R.id.playButton);
-        btnPrevious = findViewById(R.id.lastButton);
-        btnNext = findViewById(R.id.nextButton);
-        btnPlaylist = findViewById(R.id.playlistButton);
-        btnFavorite = findViewById(R.id.favoriteButton);
-        btnMusicImage = findViewById(R.id.musicImage);
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -150,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onLongClick(View view) {
                 String path = Environment.getExternalStorageDirectory().getPath();
                 File root = new File(path);
-                File [] filesAndFolders = root.listFiles();
+                filesAndFolders = root.listFiles();
                 File selectedFile = filesAndFolders[8];
                 //filesAndFolders.length;
 
@@ -191,6 +225,34 @@ public class MainActivity extends AppCompatActivity {
                 play(path);
             }
         });
+
+        btnPlaylist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_overlay_layout);
+        Button editLayout = dialog.findViewById(R.id.layoutEdit);
+
+        editLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
     }
 
     private boolean checkPermission(){
