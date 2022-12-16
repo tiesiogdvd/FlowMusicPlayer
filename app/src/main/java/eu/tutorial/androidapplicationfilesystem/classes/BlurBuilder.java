4691c88437f2d90eff170924ec5d05612f1f079e
tiesiogdvd <deivukelis@gmail.com>
2022-12-16 13:15:07 +0200

    Implemented methods to blur image bitmap for background

---
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/BlurBuilder.java
@@ -0,0 +1,73 @@
+package eu.tutorial.androidapplicationfilesystem.classes;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.view.View;
+
+
+    //Implemented class to create a blurred smooth background
+    //It downscales the image by a set variable BITMAP_SCALE
+    //Then the method upscales it
+    //Lastly the image gets blurred
+
+    public class BlurBuilder {
+        private static final float BITMAP_SCALE = 0.2f; //0.2F means the image will be downscaled to 0.2x width and height before being rescaled
+        private static final float BLUR_RADIUS = 7.5f;
+
+        public static Bitmap blur(View v) {
+            return blur(v.getContext(), getScreenshot(v));
+        }
+
+        public static Bitmap getResizedBitmap(Bitmap bitmap, int newWidth, int newHeight) {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            float scaleWidth = ((float) newWidth) / width;
+            float scaleHeight = ((float) newHeight) / height;
+            Matrix matrix = new Matrix();
+            matrix.postScale(scaleWidth, scaleHeight);
+            Bitmap resizedBitmap = Bitmap.createBitmap(
+                    bitmap, 0, 0, width, height, matrix, false);
+            return resizedBitmap;
+        }
+
+        public static Bitmap blur(Context ctx, Bitmap image) {
+            image = getResizedBitmap(image, 100, 400);
+
+            int width = Math.round(image.getWidth() * BITMAP_SCALE);
+            int height = Math.round(image.getHeight() * BITMAP_SCALE);
+
+            Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, true);
+            Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+            RenderScript rs = RenderScript.create(ctx);
+            ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+            Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+            Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+            theIntrinsic.setRadius(BLUR_RADIUS);
+            theIntrinsic.setInput(tmpIn);
+            theIntrinsic.forEach(tmpOut);
+            tmpOut.copyTo(outputBitmap);
+
+            return outputBitmap;
+        }
+
+        private static Bitmap getScreenshot(View v) {
+            Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(b);
+            v.draw(c);
+            return b;
+        }
+
+        private static Bitmap bitmapRescale(Bitmap bitmap, int width, int height){
+            Bitmap rescaledBitmap = bitmap.createScaledBitmap(bitmap, width, height,false);
+            return rescaledBitmap;
+        }
+
+    }
+
