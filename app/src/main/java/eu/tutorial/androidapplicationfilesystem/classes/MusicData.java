c06c26b76ce9de7d88f50fa7639e9ea67235b796
tiesiogdvd <deivukelis@gmail.com>
2023-01-02 15:39:01 +0200

    Modified class to reduce load and repetition of code

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MusicData.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MusicData.java
@@ -20,135 +20,73 @@ public class MusicData {
     String title;
     String album;
     Bitmap bmap;
-    MediaMetadataRetriever mr;
     File file;
     String date;
+
+
+    public void initConstructorData(File file){
+        this.file = file;
+        this.path = file.getAbsolutePath();
+        this.title = MusicDataMetadata.getTitle(file);
+        this.artist = MusicDataMetadata.getArtist(file);
+        this.album = MusicDataMetadata.getAlbum(file);
+    }
+
+    public void initConstructorData(String path){
+        this.path = path;
+        this.title = MusicDataMetadata.getTitle(path);
+        this.artist = MusicDataMetadata.getArtist(path);
+        this.album = MusicDataMetadata.getAlbum(path);
+    }
 
     public MusicData(){
     }
 
     public MusicData(File file){
-        this.file = file;
-        this.path = file.getAbsolutePath();
-        this.title = getTitle(file);
-        this.artist = getArtist(file);
-        this.album = getAlbum(file);
-        this.bmap = getBitmap(file);
+        initConstructorData(file);
         this.date = setInitialDate();
     }
 
     public MusicData(String path){
-        File file = new File(path);
-        this.file = file;
-        this.path = file.getAbsolutePath();
-        this.title = getTitle(file);
-        //System.out.println(title);
-        this.artist = getArtist(file);
-        this.album = getAlbum(file);
-        this.bmap = getBitmap(file);
+        //File file = new File(path);
+        initConstructorData(path);
         this.date = setInitialDate();
     }
 
     public MusicData(File file, String date){
-        this.file = file;
-        this.path = file.getAbsolutePath();
-        this.title = getTitle(file);
-        this.artist = getArtist(file);
-        this.album = getAlbum(file);
-        this.bmap = getBitmap(file);
+        initConstructorData(file);
         this.date = date;
     }
 
     public MusicData(String path, String date){
 
-        File file = new File(path);
-        this.file = file;
-        this.path = file.getAbsolutePath();
-        this.title = getTitle(file);
-        //System.out.println(title);
-        this.artist = getArtist(file);
-        this.album = getAlbum(file);
-        this.bmap = getBitmap(file);
+        //File file = new File(path);
+        //initConstructorData(file);
+        initConstructorData(path);
         this.date = date;
     }
 
     public String getPath() {
+        System.out.println(path);
         return path;
     }
 
-    //This method required a file to be provided
-    public String getTitle(File file){
-        path = file.getAbsolutePath();
-        mr = new MediaMetadataRetriever();
-        mr.setDataSource(file.getAbsolutePath());
-        try{
-            title = mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
-            mr.release();
-            if(title==null){throw new Exception();}
-            return title;
-        }catch(Exception e){
-            mr.release();
-            title = path.substring(path.lastIndexOf("/")).replace("/","");
-            return title;
-        }
-    }
 
     //This method is only used if MusicData(File file) constructor has been called
     public String getTitle(){
         return title;
     }
 
-    public String getArtist(File file){
-        path = file.getAbsolutePath();
-        mr = new MediaMetadataRetriever();
-        mr.setDataSource(file.getAbsolutePath());
-        try{
-            artist = mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
-            mr.release();
-            return artist;
-        }catch(Exception e){
-            mr.release();
-            return null;
-        }
-    }
     public String getArtist(){
         return artist;
-    }
-
-    public String getAlbum(File file){
-        path = file.getAbsolutePath();
-        mr = new MediaMetadataRetriever();
-        mr.setDataSource(file.getAbsolutePath());
-        try{
-            album = mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-            mr.release();
-            return album;
-        }catch(Exception e){
-            mr.release();
-            return null;
-        }
     }
 
     public String getAlbum(){
         return album;
     }
 
-    public Bitmap getBitmap(File file){
-        path = file.getAbsolutePath();
-        mr = new MediaMetadataRetriever();
-        mr.setDataSource(file.getAbsolutePath());
-        try{
-            byte [] data = mr.getEmbeddedPicture();
-            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-            mr.release();
-            return bitmap;
-        }catch(Exception e){
-            mr.release();
-            return null;
-        }
-    }
-
     public Bitmap getBitmap(){
+        this.bmap = MusicDataMetadata.getBitmap(new File(path));
         return bmap;
     }
 
@@ -165,7 +103,6 @@ public class MusicData {
     }
 
     private Date setDateJavaUtil(String dateString){
-
         String dateFormat = "dd/MM/Y hh:mm:ss a";
         SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
         Date date = null;
