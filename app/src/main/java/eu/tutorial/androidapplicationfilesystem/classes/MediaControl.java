e627a8efb0688a315809170be55419517244becc
tiesiogdvd <deivukelis@gmail.com>
2023-01-02 15:34:35 +0200

    Implemented checking of song and position after application was killed

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MediaControl.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MediaControl.java
@@ -8,101 +8,53 @@ Implements the MetadataGetterSetter class to set the title, artist and bitmap im
 
 package eu.tutorial.androidapplicationfilesystem.classes;
 
-import android.annotation.SuppressLint;
+
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.os.Handler;
 import android.os.IBinder;
 import android.view.View;
-import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-
-import java.util.concurrent.TimeUnit;
-
 import eu.tutorial.androidapplicationfilesystem.R;
 import eu.tutorial.androidapplicationfilesystem.activities.MainActivity;
 import soup.neumorphism.NeumorphImageButton;
 
 public class MediaControl extends AppCompatActivity {
 
-    NeumorphImageButton btnPlay, btnPrevious, btnNext;
-    final Handler handler;
-    SeekBar seekBar;
-    TextView totalText,remainingText;
+    NeumorphImageButton btnPlay;
     Context context;
-    Runnable runnable;
-
-    BroadcastReceiver receiver;
     MediaControlService mediaControlService;
     boolean isServiceBound;
     ServiceConnection serviceConnection;
     Intent serviceIntent;
+    Intent intentBroadcast;
+    int lastPosition;
+    String lastSong;
 
 
     public MediaControl(Context context) {
+        this.lastSong = null;
         this.isServiceBound = false;
-        this.handler =  new Handler();
-        this.seekBar = null;
-        this.btnPrevious = ((MainActivity)context).findViewById(R.id.lastButton);
-        this.btnNext = ((MainActivity)context).findViewById(R.id.nextButton);
-        this.btnPlay = ((MainActivity)context).findViewById(R.id.playButton);
-        this.seekBar = ((MainActivity)context).findViewById(R.id.musicProgress);
-        this.totalText = ((MainActivity)context).findViewById(R.id.musicTotalText);
-        this.remainingText = ((MainActivity)context).findViewById(R.id.musicRemainingText);
-
+        this.btnPlay = ((MainActivity)context).findViewById(R.id.playButtonBar);
         this.context = context;
         serviceIntent = new Intent(context, MediaControlService.class);
-        //startService();
         bindService();
         setListeners();
     }
 
-    public void setRunnable(){//This runnable moves seekbar at a selected interval
-        setOnSeekbarListener(); //Actives seekbar listener method
-        if (mediaControlService.isMediaReady()){ //Only runs the code if it detects that the service is playing audio
-            System.out.println(mediaControlService.mediaRemaining());
-            totalText.setText(formatDuration(mediaControlService.mediaLastDuration()));
-            remainingText.setText(formatDuration(mediaControlService.mediaLastPosition()));
-            seekBar.setMax(mediaControlService.mediaLastDuration());
-            seekBar.setProgress(mediaControlService.mediaLastPosition());
-            if(mediaControlService.isMediaPlaying()) {
 
-                handler.removeCallbacks(runnable);
-                runnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        seekBar.setProgress(mediaControlService.mediaRemaining()); //Requests the service position of MediaPlayer every 0.5s
-                        System.out.println("Runnable instance " + handler.toString());
-                        handler.postDelayed(this, 1000);
-                    }
-                };
-                handler.postDelayed(runnable, 0);
-            }
-        }
-    }
-
-    public void startService(){
-
-        //startService(serviceIntent);
-    }
 
     public void play(String path) {
         try{
             if(path.equals(null)){
                 throw new Exception();
             }else{
-                setMetadata(path);
                 mediaControlService.playMedia(path);
                 btnPlay.setImageResource(R.drawable.ic_action_pause);
-                setRunnable();
                 //isFinished();
             }
         }catch(Exception e){
@@ -110,24 +62,9 @@ public class MediaControl extends AppCompatActivity {
         }
     }
 
-    public void setMetadata(String path){
-        MetadataGetterSetter metadataGetterSetter = new MetadataGetterSetter();
-        //MetadataGetterSetter class is called to receive metadata with the help of MusicData class and then put it into activity_main Views
-        metadataGetterSetter.retrieveMetadata(path, context);
-        //For it to work it is required the audio file path and MainActivity context
-    }
-
-
-    /*public void initService(){
-        System.out.println("bleeed");
-        serviceIntent = new Intent(context, MediaControlService.class);
-        serviceIntent.setAction("create");
-        ContextCompat.startForegroundService(context, serviceIntent);
-    }*/
 
 
     public void bindService(){
-
         serviceIntent.setAction("create");
         ContextCompat.startForegroundService(context, serviceIntent);
 
@@ -138,29 +75,31 @@ public class MediaControl extends AppCompatActivity {
                     MediaControlService.MyServiceBinder myServiceBinder = (MediaControlService.MyServiceBinder)iBinder;
                     mediaControlService = myServiceBinder.getService();
                     isServiceBound = true;
-
                     //bindService method is not instant
                     //For that reason code for runnable is activated only after the service is binded
                     //Otherwise methods might be called before the bind and cause crashes
 
                     if(!mediaControlService.musicPath.equals("none")){ //checks if there was music played before
                         String path = mediaControlService.musicPath; //important on activity changes like rotation
-                        setMetadata(path); //sets metadata to last played song
+                        MetadataGetterSetter.setBarMetadata(context,path);
                         if(mediaControlService.isMediaPlaying()){
                             btnPlay.setImageResource(R.drawable.ic_action_pause);
-                            setRunnable();
                         }else{
-
-
+                            //StyleSetter.setInitBackground(context);
+                        }
+                    }else{
+                        if(lastSong!="" && lastPosition != -1 && lastSong != null){
+                            mediaControlService.playMedia(lastSong);
+                            mediaControlService.mediaSeekTo(lastPosition);
+                            mediaControlService.pauseMedia();
+                            btnPlay.setImageResource(R.drawable.ic_action_play);
+                            MetadataGetterSetter.setBarMetadata(context,lastSong);
                         }
                     }
-
-
                 }
                 @Override
                 public void onServiceDisconnected(ComponentName name) {
                     isServiceBound=false;
-                    handler.removeCallbacksAndMessages(null);
                 }
             };
         }
@@ -177,16 +116,22 @@ public class MediaControl extends AppCompatActivity {
 
 
 
+    private void broadcast(String key, String payload){
+        intentBroadcast = new Intent();
+        intentBroadcast.putExtra(key, payload);
+        intentBroadcast.setAction("ASK");
+        intentBroadcast.setFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        context.sendBroadcast(intentBroadcast);
+    }
+
     public void play(){
         mediaControlService.playMedia();
-        setRunnable();
         btnPlay.setImageResource(R.drawable.ic_action_pause);
 
     }
 
     public void pause(){
         mediaControlService.pauseMedia();
-        handler.removeCallbacks(runnable);
         btnPlay.setImageResource(R.drawable.ic_action_play);
     }
 
@@ -195,10 +140,7 @@ public class MediaControl extends AppCompatActivity {
         stopService(serviceIntent);
     }
 
-
-
     public void play(int resid) { //Play music from app resource
-        handler.postDelayed(runnable, 0);
         mediaControlService.playMedia(resid);
         btnPlay.setImageResource(R.drawable.ic_action_pause);
     }
@@ -207,112 +149,51 @@ public class MediaControl extends AppCompatActivity {
         return mediaControlService.isMediaPlaying();
     }
 
-
-    @SuppressLint("DefaultLocale")
-    private String convertTimeFormat(int duration){
-        return String.format("%02d:%02d",
-                TimeUnit.MILLISECONDS.toMinutes(duration),
-                TimeUnit.MILLISECONDS.toSeconds(duration) -
-                        (TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))));
-    }
-
-    private String formatDuration(int duration) {
-        int minutes = (int) TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS);
-        int seconds = (int) (TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS)
-                - minutes * TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES));
-
-        return String.format("%02d:%02d", minutes, seconds);
-    }
-
-
-    public void isFinished(){
-        unregisterReceiver(receiver);
-        receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Boolean isFinished = intent.getExtras().getBoolean("isFinished");
-                System.out.println(isFinished);
-            }
-        };
-        IntentFilter filt = new IntentFilter("ANSWER");
-        context.registerReceiver(receiver,filt);
-    }
-
-     /*  if(BroadcastReceiverMediaPlayer.isFinished(context)){
-        mediaControlService.pauseMedia();
-        System.out.println("IS FINISHED");
-        mediaControlService.playMedia();
-    }*/
-
-    public void unsetOnSeekbarListener(){
-        if(seekBar!=null) {
-            seekBar.setOnSeekBarChangeListener(null);
-        }
-    }
-
     public void handlerRemoveCallbacks(){
-        handler.removeCallbacksAndMessages(null);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        handler.removeCallbacksAndMessages(null);
     }
 
-    private void setOnSeekbarListener(){
-        this.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                if (b){ //If from user
-                    mediaControlService.mediaSeekTo(progress);
-                }
-                if(isServiceBound){
-                    remainingText.setText(convertTimeFormat(mediaControlService.mediaRemaining()));
-                }
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
 
-        });
+    public String getPath(){
+        return mediaControlService.musicPath;
     }
 
-    private void setListeners(){
+    public int getRemaining(){
+        return mediaControlService.mediaRemaining();
+    }
+
+    public void seekTo(int position){
+        mediaControlService.mediaSeekTo(position);
+    }
+
+    private void setListeners() {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 //savePlaylistData();
-                if(mediaControlService!=null){
-                    if(!mediaControlService.isMediaPlaying()) {
+                if (mediaControlService != null) {
+                    if (!mediaControlService.isMediaPlaying()) {
                         play();
                     } else {
                         pause();
                     }
-                } else{
+                } else {
                     Toast.makeText(context, "No music resource loaded", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        btnPrevious.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(isServiceBound) {
-                    System.out.println(mediaControlService.isMediaPlaying());
-                    mediaControlService.playMedia();
-                }
-
-            }
-        });
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
 
 
-            }
-        });
+    public void setLastSong(String lastSongPath) {
+        this.lastSong = lastSongPath;
+    }
+
+    public void setLastPosition(int lastSongPosition) {
+        this.lastPosition = lastSongPosition;
     }
 }
