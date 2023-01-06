2172f7ec013e7ef22b873cbc2df25e2397bc76b4
tiesiogdvd <deivukelis@gmail.com>
2023-01-06 21:54:58 +0200

    Implemnted single static method to get metadata for performance

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MusicDataMetadata.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MusicDataMetadata.java
@@ -1,19 +1,74 @@
 package eu.tutorial.androidapplicationfilesystem.classes;
 
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
+import android.provider.MediaStore;
 
 import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 
 //This class implements storing music data and creates methods to retrieve Metadata from it
 
 public class MusicDataMetadata {
+    public static String title;
+    public static String artist;
+    public static String album;
+    public static Bitmap bitmap;
+    public static int length;
+    public static String lengthString;
+    public static MediaMetadataRetriever mr = new MediaMetadataRetriever();
+
+
+
+    public static void setAllData(String path){
+        //MediaMetadataRetriever mr = new MediaMetadataRetriever();
+        mr = new MediaMetadataRetriever();
+        mr.setDataSource(path);
+
+        try{
+            title = mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+            if(title==null){throw new Exception();}
+        }catch(Exception e){
+            title = path.substring(path.lastIndexOf("/")).replace("/","");
+        }
+
+        try{
+            artist = mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        }catch(Exception e){
+
+        }
+
+        try{
+            album = mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+        }catch(Exception e){
+
+        }
+
+        try{
+            byte [] data = mr.getEmbeddedPicture();
+            bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+        }catch(Exception e){
+
+        }
+
+        try{
+            length = Integer.parseInt(mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            lengthString = convertTimeFormat(length);
+        }catch(Exception e){
+
+        }
+
+        mr.release();
+    }
+
+
 
     //This method required a file to be provided
     public static String getTitle(File file){
@@ -50,6 +105,7 @@ public class MusicDataMetadata {
 
     public static String getArtist(File file){
         String path = file.getAbsolutePath();
+        MediaStore.Audio.Albums.getContentUri(file.getAbsolutePath());
         MediaMetadataRetriever mr = new MediaMetadataRetriever();
         mr.setDataSource(path);
         try{
@@ -131,5 +187,69 @@ public class MusicDataMetadata {
             return null;
         }
     }
+
+    public static String getLengthString(String path) {
+        MediaMetadataRetriever mr = new MediaMetadataRetriever();
+        mr.setDataSource(path);
+        try{
+            int length = Integer.parseInt(mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            mr.release();
+            String lengthString = convertTimeFormat(length);
+            return lengthString;
+        }catch(Exception e){
+            mr.release();
+            return "";
+        }
+    }
+
+    public static String getLengthString(File file) {
+        MediaMetadataRetriever mr = new MediaMetadataRetriever();
+        mr.setDataSource(file.getAbsolutePath());
+        try{
+            int length = Integer.parseInt(mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            mr.release();
+            String lengthString = convertTimeFormat(length);
+            return lengthString;
+        }catch(Exception e){
+            mr.release();
+            return "";
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    private static String convertTimeFormat(int duration){
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(duration),
+                TimeUnit.MILLISECONDS.toSeconds(duration) -
+                        (TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))));
+    }
+
+    public static int getLengthInt(String path) {
+        MediaMetadataRetriever mr = new MediaMetadataRetriever();
+        mr.setDataSource(path);
+        try{
+            int length = Integer.parseInt(mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            mr.release();
+            return length;
+        }catch(Exception e){
+            mr.release();
+            return 0;
+        }
+    }
+
+    public static int getLengthInt(File file) {
+        MediaMetadataRetriever mr = new MediaMetadataRetriever();
+        mr.setDataSource(file.getAbsolutePath());
+        try{
+            int length = Integer.parseInt(mr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            mr.release();
+            return length;
+        }catch(Exception e){
+            mr.release();
+            return 0;
+        }
+    }
+
+
 
 }
