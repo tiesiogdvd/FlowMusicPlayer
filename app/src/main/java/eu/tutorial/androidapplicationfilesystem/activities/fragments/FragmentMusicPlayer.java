08ab2a7917559bbbbdd95ec503220b54c8def245
tiesiogdvd <deivukelis@gmail.com>
2023-01-06 20:15:06 +0200

    Code cleanup and reorganizing

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/activities/fragments/FragmentMusicPlayer.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/activities/fragments/FragmentMusicPlayer.java
@@ -8,7 +8,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +19,6 @@ import eu.tutorial.androidapplicationfilesystem.R;
 import eu.tutorial.androidapplicationfilesystem.classes.DialogPlaylist;
 import eu.tutorial.androidapplicationfilesystem.classes.MediaControlFragment;
 import eu.tutorial.androidapplicationfilesystem.classes.Playlist;
-import eu.tutorial.androidapplicationfilesystem.classes.PlaylistDatabaseHelper;
 import eu.tutorial.androidapplicationfilesystem.classes.ViewModelMain;
 import eu.tutorial.androidapplicationfilesystem.interfaces.PassMusicStatus;
 import soup.neumorphism.NeumorphImageButton;
@@ -28,93 +26,70 @@ import soup.neumorphism.NeumorphImageButton;
 
 public class FragmentMusicPlayer extends Fragment{
 
-    PlaylistDatabaseHelper myDB;
     ArrayList<Playlist> playlists;
     NeumorphImageButton btnPlaylist;
-    DialogPlaylist dialog;
-    File musicFile;
-    MediaControlFragment mc;
-    PassMusicStatus passDataInterface;
-    private ViewModelMain viewModelMain;
+    NeumorphImageButton favButton;
+    DialogPlaylist dialog; //dialog for adding songs to playlists and removing them
+    File musicFile; //file that is currently playing
+    MediaControlFragment mc; //MediaControl class binds application to service for MediaPlayer
+    PassMusicStatus passDataInterface; //Gets passed to MediaControl class to create communication between MainActivity and fragment about music status
 
     public FragmentMusicPlayer() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setEnterTransition(TransitionInflater.from(getContext()).inflateTransition(android.R.transition.fade));
-        //setSharedElementEnterTransition(TransitionInflater.from(getContext()).inflateTransition(android.R.transition.move));
-       // setSharedElementEnterTransition(TransitionInflater.from(getContext()).inflateTransitionManager(R.transition.image_transition, getView()));
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof PassMusicStatus){ //Checks if there is an implementation of PassMusicStatus in the context of parent activity
+            passDataInterface = (PassMusicStatus) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement PassData");
+        }
     }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        playlists = new ArrayList<>();
-        viewModelMain = new ViewModelProvider(requireActivity()).get(ViewModelMain.class);
-        viewModelMain.getPlaylists().observe(getViewLifecycleOwner(),playlistsViewModel -> {
-            if(playlistsViewModel!=null){
-                playlists.clear();
-                playlists.addAll(playlistsViewModel);
-            }
-        });
-
-        /*if (getArguments() != null){
-            playlists = (ArrayList<Playlist>)getArguments().getSerializable("playlists");
-            System.out.println("hahahahahah");
-            musicFile = (File) getArguments().getSerializable("musicFile");
-        } else {
-
-            myDB = new PlaylistDatabaseHelper(getContext());
-            playlists = new ArrayList<>();
-            getPlaylists();
-        }*/
-
-        return inflater.inflate(R.layout.fragment_music_player, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_music_player, container, false);
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //StyleSetter.setInitBackground(getContext());
         initViews(view);
+        initViewModel();
         initListeners();
-        //SwipeListener swipeListener = new SwipeListener(this,c)
-        //if mc.is
     }
 
 
-
-
-    public void initViews(View view){
+    private void initViews(View view){
+        playlists = new ArrayList<>();
         mc = new MediaControlFragment(getContext());
         mc.setPassDataInterface(passDataInterface);
-        System.out.println("Shit happens");
-        dialog = new DialogPlaylist();
-        //musicFile = new File("/storage/emulated/0/MuzikaTest/Rammstein - Rammstein (2019) [320]/08 DIAMANT.mp3"); //keep temporarily to keep playlists working until data retrieved from mainactivity
-        //myDB = new PlaylistDatabaseHelper(getContext());
-        //playlists = new ArrayList<>();
-        //getPlaylists();
-        btnPlaylist = view.findViewById(R.id.playlistButton);
-
         mc.bindService();
+        dialog = new DialogPlaylist();
+        btnPlaylist = view.findViewById(R.id.playlistButton);
+        favButton = view.findViewById(R.id.favoriteButton);
+    }
 
-        NeumorphImageButton favButton = view.findViewById(R.id.favoriteButton);
-        favButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //passDataInterface.onDataReceived(mc.isPlaying());
+    private void initViewModel(){
+        ViewModelMain viewModelMain = new ViewModelProvider(requireActivity()).get(ViewModelMain.class);
+        viewModelMain.getPlaylists().observe(getViewLifecycleOwner(), playlistsViewModel -> { //Created observable checks for playlist changes in ViewModel playlists LiveData
+            if(playlistsViewModel!=null){
+                playlists.clear(); //Upon change of playlists in ViewModel it resets the playlists data in this fragment.
+                playlists.addAll(playlistsViewModel);
             }
         });
     }
 
 
-    public void initListeners(){
+    private void initListeners(){
         btnPlaylist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -122,12 +97,14 @@ public class FragmentMusicPlayer extends Fragment{
             }
         });
 
-    }
+        favButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public void lastPlayed(File file){
-        musicFile = file;
-    }
+            }
+        });
 
+    }
 
     @Override
     public void onDestroy() {
@@ -154,21 +131,17 @@ public class FragmentMusicPlayer extends Fragment{
 
 
     @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if(context instanceof PassMusicStatus){
-            passDataInterface = (PassMusicStatus) context;
-        } else {
-            throw new RuntimeException(context.toString() + " must implement PassData");
-        }
-    }
-
-    @Override
     public void onDetach() {
         super.onDetach();
         passDataInterface=null;
         mc.unsetPassDataInterface();
         mc.handlerRemoveCallbacks();
     }
+
+
+    public void lastPlayed(File file){
+        musicFile = file;
+    }
+
 
 }
