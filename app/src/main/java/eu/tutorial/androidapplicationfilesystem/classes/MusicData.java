e6194dcd4d4bb58c0fbca4f3267db58149e23b1c
tiesiogdvd <deivukelis@gmail.com>
2023-01-06 21:54:34 +0200

    Testing extra methods for less performance waste

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MusicData.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MusicData.java
@@ -1,14 +1,14 @@
 package eu.tutorial.androidapplicationfilesystem.classes;
 
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
 
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-import java.io.File;
 import java.util.Date;
 
 
@@ -16,9 +16,11 @@ import java.util.Date;
 
 public class MusicData {
     String path;
-    String artist;
+    String cachedImagePath;
     String title;
+    String artist;
     String album;
+    int lengthInt;
     Bitmap bmap;
     File file;
     String date;
@@ -27,30 +29,73 @@ public class MusicData {
     public void initConstructorData(File file){
         this.file = file;
         this.path = file.getAbsolutePath();
-        this.title = MusicDataMetadata.getTitle(file);
-        this.artist = MusicDataMetadata.getArtist(file);
-        this.album = MusicDataMetadata.getAlbum(file);
+        MusicDataMetadata.setAllData(path);
+        this.title = MusicDataMetadata.title;
+        this.artist = MusicDataMetadata.artist;
+        this.album = MusicDataMetadata.album;
+        this.bmap = MusicDataMetadata.bitmap;
+        this.lengthInt = MusicDataMetadata.length;
+        MusicDataMetadata.title = null;
+        MusicDataMetadata.artist = null;
+        MusicDataMetadata.album = null;
+        MusicDataMetadata.bitmap = null;
+        MusicDataMetadata.length = -1;
+        MusicDataMetadata.lengthString = null;
+        //MusicDataMetadata.mr.release();
     }
 
     public void initConstructorData(String path){
         this.path = path;
-        this.title = MusicDataMetadata.getTitle(path);
-        this.artist = MusicDataMetadata.getArtist(path);
-        this.album = MusicDataMetadata.getAlbum(path);
+        MusicDataMetadata.setAllData(path);
+        this.title = MusicDataMetadata.title;
+        this.artist = MusicDataMetadata.artist;
+        this.album = MusicDataMetadata.album;
+        if(this.bmap==null) {
+            this.bmap = MusicDataMetadata.bitmap;
+        }
+        this.lengthInt = MusicDataMetadata.length;
+        MusicDataMetadata.title = null;
+        MusicDataMetadata.artist = null;
+        MusicDataMetadata.album = null;
+        MusicDataMetadata.bitmap = null;
+        MusicDataMetadata.length=-1;
+        MusicDataMetadata.lengthString = null;
     }
 
     public MusicData(){
+        this.date = setInitialDate();
     }
 
-    public MusicData(File file){
+    public MusicData(File file, Context context){
+        initConstructorData(file.getAbsolutePath());
+        this.date = setInitialDate();
+        ImageSaver imageSaver = new ImageSaver(context);
+        imageSaver.setFileName(file.getAbsolutePath()).setDirectory("ImageCache").save(bmap);
         initConstructorData(file);
         this.date = setInitialDate();
     }
 
-    public MusicData(String path){
+    public MusicData(String path, Context context){
         //File file = new File(path);
-        initConstructorData(path);
         this.date = setInitialDate();
+        ImageSaver imageSaver = new ImageSaver(context);
+        this.bmap = imageSaver.setFileName(path).setDirectory("ImageCache").load();
+        initConstructorData(path);
+
+        this.cachedImagePath = imageSaver.setFileName(path).setDirectory("ImageCache").save(bmap);
+    }
+
+    public Bitmap loadBmap(Context context){
+        ImageSaver imageSaver = new ImageSaver(context);
+        return imageSaver.setFileName(path).setDirectory("ImageCache").load();
+    }
+
+    public String getCachedImagePath() {
+        return cachedImagePath;
+    }
+
+    public void setCachedImagePath(String cachedImagePath) {
+        this.cachedImagePath = cachedImagePath;
     }
 
     public MusicData(File file, String date){
@@ -59,11 +104,26 @@ public class MusicData {
     }
 
     public MusicData(String path, String date){
-
-        //File file = new File(path);
-        //initConstructorData(file);
         initConstructorData(path);
         this.date = date;
+    }
+
+    public MusicData(String path, String title, String artist, String album, int lengthInt, String date){
+        this.path = path;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.lengthInt = lengthInt;
+        this.date = date;
+    }
+
+    public MusicData(String path, String title, String artist, String album, int lengthInt){
+        this.path = path;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.lengthInt = lengthInt;
+        this.date = setInitialDate();
     }
 
     public String getPath() {
@@ -77,6 +137,7 @@ public class MusicData {
         return title;
     }
 
+
     public String getArtist(){
         return artist;
     }
@@ -86,7 +147,7 @@ public class MusicData {
     }
 
     public Bitmap getBitmap(){
-        this.bmap = MusicDataMetadata.getBitmap(new File(path));
+        //this.bmap = MusicDataMetadata.getBitmap(new File(path));
         return bmap;
     }
 
@@ -114,4 +175,7 @@ public class MusicData {
         return date;
     }
 
+    public int getLength() {
+        return lengthInt;
+    }
 }
