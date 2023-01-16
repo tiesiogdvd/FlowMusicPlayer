76c7e7f5573604ae26d747948738939131deecac
tiesiogdvd <deivukelis@gmail.com>
2023-01-16 11:34:38 +0200

    Implemented more interface methods to pass data to MainActivity

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/interfaces/PassMusicStatus.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/interfaces/PassMusicStatus.java
@@ -1,5 +1,12 @@
 package eu.tutorial.androidapplicationfilesystem.interfaces;
 
+import eu.tutorial.androidapplicationfilesystem.activities.fragmentsMainActivity.FragmentLibrarySongs;
+import eu.tutorial.androidapplicationfilesystem.classes.Playlist;
+
 public interface PassMusicStatus {
     void onDataReceived(Boolean isPlaying);
+    void onSongRequest(String songPath, Playlist playlist, int index);
+    void onMediaReady(Boolean isReady);
+    void onLibraryStoragePressed();
+    void onRequestNavbar(Boolean request, FragmentLibrarySongs fragmentLibrarySongs);
 }
