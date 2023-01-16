c90d9e25d1fcf4573dc83d9da48c0dc604d1b1a5
tiesiogdvd <deivukelis@gmail.com>
2023-01-16 11:44:09 +0200

    Edited notification channels

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/ServiceNotification.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/ServiceNotification.java
@@ -1,15 +1,17 @@
 package eu.tutorial.androidapplicationfilesystem.classes;
 
 import android.app.Application;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 
 public class ServiceNotification extends Application {
-    public static final String CHANNEL_ID_1 = "mediaPlayerChannel";
-    public static final String CHANNEL_ID_2 = "mediaPlayerChannel";
-    public static final String ACTION_PREVIOUS = "actionPrevious";
-    public static final String ACTION_NEXT = "actionNext";
-    public static final String ACTION_PLAY = "actionPlay";
+
+    public static final String CHANNEL_ID_1 = "mediaPlayerChannel1";
+    public static final String CHANNEL_ID_2 = "mediaPlayerChannel2";
+    public static final String ACTION_PREVIOUS = "PREVIOUS";
+    public static final String ACTION_NEXT = "NEXT";
+    public static final String ACTION_PLAY = "PLAY";
 
 
     @Override
@@ -19,9 +21,12 @@ public class ServiceNotification extends Application {
     }
     private void createNotificationChannel(){
 
-
-        NotificationChannel serviceChannel1 = new NotificationChannel(CHANNEL_ID_1, "Example Service Channel", NotificationManager.IMPORTANCE_DEFAULT);
-        NotificationChannel serviceChannel2 = new NotificationChannel(CHANNEL_ID_2, "Example Service Channel", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationChannel serviceChannel1 = new NotificationChannel(CHANNEL_ID_1, "MP Channel 1", NotificationManager.IMPORTANCE_HIGH);
+        serviceChannel1.setDescription("MP Channel 1 description");
+        serviceChannel1.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        NotificationChannel serviceChannel2 = new NotificationChannel(CHANNEL_ID_2, "MP Channel 2", NotificationManager.IMPORTANCE_DEFAULT);
+        serviceChannel2.setDescription("MP Channel 2 description");
+        serviceChannel2.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
         NotificationManager manager = getSystemService(NotificationManager.class);
         manager.createNotificationChannel(serviceChannel1);
         manager.createNotificationChannel(serviceChannel2);
