ee7a3b85607890bf8b27223238550049a573f67f
tiesiogdvd <deivukelis@gmail.com>
2023-01-16 10:50:52 +0200

    Added methods to display playlists and interact with them

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/adapters/AdapterPlaylistsLibrary.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/adapters/AdapterPlaylistsLibrary.java
@@ -1,45 +1,49 @@
 package eu.tutorial.androidapplicationfilesystem.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 import eu.tutorial.androidapplicationfilesystem.R;
+import eu.tutorial.androidapplicationfilesystem.activities.fragmentsMainActivity.FragmentLibrarySongs;
 import eu.tutorial.androidapplicationfilesystem.classes.Playlist;
-import eu.tutorial.androidapplicationfilesystem.classes.ViewModelMain;
 
 public class AdapterPlaylistsLibrary extends RecyclerView.Adapter <AdapterPlaylistsLibrary.ViewHolder> {
 
     Context context;
     ArrayList<Playlist> musicPlaylists;
+    ArrayList<Playlist> musicPlaylistsCopy;
+    String libraryFragmentPlaylistTag = "playlist";
 
     public AdapterPlaylistsLibrary(Context context, ArrayList<Playlist> musicPlaylists){
     this.context = context;
     this.musicPlaylists = musicPlaylists;
+    this.musicPlaylistsCopy = musicPlaylists;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView playlistName;
         ImageView bitmap;
-
+        LinearLayout linearLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             playlistName = itemView.findViewById(R.id.textLibraryPlaylist);
             bitmap = itemView.findViewById(R.id.imageLibraryPlaylist);
+            linearLayout = itemView.findViewById(R.id.layoutLibraryPlaylist);
         }
     }
 
@@ -48,18 +52,61 @@ public class AdapterPlaylistsLibrary extends RecyclerView.Adapter <AdapterPlayli
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.recycler_item_library, parent, false);
-        return new ViewHolder(view);
+        final ViewHolder holder= new ViewHolder(view);
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Playlist selectedPlaylist = musicPlaylists.get(holder.getAdapterPosition());
+                FragmentManager fragmentManager = ((FragmentActivity)context).getSupportFragmentManager();
+                FragmentLibrarySongs fragmentLibrarySongs = new FragmentLibrarySongs();
+                System.out.println(selectedPlaylist.getPlaylistName());
+                Bundle bundle = new Bundle();
+                bundle.putString("action","playlist");
+                bundle.putString("playlist",selectedPlaylist.getPlaylistName());
+                fragmentLibrarySongs.setArguments(bundle);
+                fragmentManager.beginTransaction()
+                        // .setCustomAnimations(R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+                        .setCustomAnimations(R.anim.pop_in, R.anim.fade_out, R.anim.pop_in, R.anim.fade_out)
+                        .replace(R.id.fragmentContainerView, fragmentLibrarySongs, libraryFragmentPlaylistTag)
+                        .addToBackStack(libraryFragmentPlaylistTag)
+                        //.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        //.setReorderingAllowed(true)
+                        .commit();
+            }
+        });
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
         Playlist selectedPlaylist = musicPlaylists.get(position);
         holder.playlistName.setText(selectedPlaylist.getPlaylistName());
         holder.bitmap.setImageBitmap(selectedPlaylist.getPlaylistBitmap(context));
+
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void filter(String text){
+        musicPlaylistsCopy = null;
+        musicPlaylistsCopy = new ArrayList<>();
+        if(text.isEmpty()){
+            musicPlaylistsCopy = musicPlaylists;
+        } else {
+            text = text.toLowerCase();
+            for(Playlist playlist: musicPlaylists){
+                if(playlist.getPlaylistName().contains(text)){
+                    musicPlaylistsCopy.add(playlist);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 
     @Override
     public int getItemCount() {
+        System.out.println(musicPlaylists.size());
         return musicPlaylists.size();
     }
 
