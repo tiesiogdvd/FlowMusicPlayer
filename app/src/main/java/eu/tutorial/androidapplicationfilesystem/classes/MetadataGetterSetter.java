01188f29aa0c6b33dccd0e7202700fb31334ed28
tiesiogdvd <deivukelis@gmail.com>
2023-01-26 15:25:58 +0200

    AsyncTask replaced by Executor, added artist to MusicBar

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MetadataGetterSetter.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/MetadataGetterSetter.java
@@ -4,105 +4,96 @@ package eu.tutorial.androidapplicationfilesystem.classes;
 //Used to set metadata to music player fragment views
 //Makes use of MusicData class to get the Metadata
 
-import android.content.AsyncQueryHandler;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.loader.content.AsyncTaskLoader;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.android.material.imageview.ShapeableImageView;
 
 import java.io.File;
+import java.util.List;
+import java.util.concurrent.Executors;
 
 import eu.tutorial.androidapplicationfilesystem.R;
 import eu.tutorial.androidapplicationfilesystem.activities.MainActivity;
-import eu.tutorial.androidapplicationfilesystem.adapters.AdapterLibraryAllSongs;
-import soup.neumorphism.NeumorphButton;
+import linc.com.amplituda.Cache;
+import linc.com.amplituda.Compress;
+import linc.com.amplituda.exceptions.io.AmplitudaIOException;
 import soup.neumorphism.NeumorphImageButton;
 
 public class MetadataGetterSetter {
     TextView songName;
+    TextView barText;
+    TextView barArtist;
     TextView artistName;
     NeumorphImageButton btnMusicImage;
+    ShapeableImageView barImage;
     File musicFile;
     MusicData musicData;
 
-    public static void retrieveMetadata(String path, Context context) {  //Song path and mainactivity context
-        System.out.println("Retrieving");
-        TextView songName = ((MainActivity) context).findViewById(R.id.musicSongName);
-        TextView artistName = ((MainActivity) context).findViewById(R.id.musicSongArtist);
-        NeumorphImageButton btnMusicImage = ((MainActivity) context).findViewById(R.id.musicImage);
-        File musicFile = new File(path);
+    public void retrieveMetadata(String path, Context context) {  //Song path and mainactivity context
+        songName = ((MainActivity) context).findViewById(R.id.musicSongName);
+        artistName = ((MainActivity) context).findViewById(R.id.musicSongArtist);
+        btnMusicImage = ((MainActivity) context).findViewById(R.id.musicImage);
+        musicFile = new File(path);
 
-        TextView barText = ((MainActivity) context).findViewById(R.id.musicSongNameBar);
-        ShapeableImageView barImage = ((MainActivity) context).findViewById(R.id.musicImageBar);
+        barText = ((MainActivity) context).findViewById(R.id.musicSongNameBar);
+        barArtist = ((MainActivity) context).findViewById(R.id.musicSongArtistBar);
+        barImage = ((MainActivity) context).findViewById(R.id.musicImageBar);
 
         if (musicFile.exists()) {
-            MusicDataMetadata musicDataMetadata = new MusicDataMetadata();
-            musicDataMetadata.setAllData(musicFile.getAbsolutePath());
-            class retrievingMetadata extends AsyncTask <Void, Void, MusicDataMetadata>{
-                String m_path;
-                public retrievingMetadata(String path){
-                    this.m_path = path;
-                }
+            Executors.newSingleThreadExecutor().execute(() -> {
+                // todo: background tasks
+                MusicDataMetadata musicDataMetadata = new MusicDataMetadata();
+                musicDataMetadata.setAllData(musicFile.getAbsolutePath());
 
-                @Override
-                protected MusicDataMetadata doInBackground(Void... voids) {
-                    MusicDataMetadata musicDataMetadata = new MusicDataMetadata();
-                    musicDataMetadata.setAllData(m_path);
-                    return musicDataMetadata;
-                }
-
-                @Override
-                protected void onPostExecute(MusicDataMetadata musicDataMetadata) {
-                    super.onPostExecute(musicDataMetadata);
+                ((MainActivity) context).runOnUiThread(()->{
                     String title = musicDataMetadata.title;
-                    System.out.println(title + " SONG NAME REQUEST");
                     String album = musicDataMetadata.album;
                     String artist = musicDataMetadata.artist;
                     Bitmap bitmap = musicDataMetadata.bitmap;
-                    //Implementing the changing of views in MainActivity
+                    //Implementing the changing of views in activity
                     songName.setText("Title: " + title);
                     barText.setText(title);
                     if (artist != null) {
                         artistName.setVisibility(View.VISIBLE);
                         artistName.setText("Artist: " + artist);
+                        barArtist.setVisibility(View.VISIBLE);
+                        barArtist.setText(artist);
                     } else {
                         artistName.setVisibility(View.INVISIBLE);
-                    }
-                    if (bitmap != null) {
-                        btnMusicImage.setImageBitmap(bitmap);
-                        barImage.setImageBitmap(bitmap);
-                        StyleSetter.setStyles(context, bitmap);
-
-                    } else {
-                        barImage.setImageResource(R.drawable.ic_group_23_image_6);
-                        btnMusicImage.setImageResource(R.drawable.ic_action_note);
-                        StyleSetter.setStyles(context, null);
+                        barArtist.setVisibility(View.GONE);
                     }
 
-                }
-            }
-            new retrievingMetadata(musicFile.getAbsolutePath()).execute();
+                    if(!Settings.getBackgroundMode()){ //Background mode square image
+                        if (bitmap != null) {
+                            btnMusicImage.setImageBitmap(bitmap);
+                            barImage.setImageBitmap(bitmap);
+                            StyleSetter.setStyles(context, bitmap);
+                        } else {
+                            btnMusicImage.setImageResource(R.drawable.ic_action_note);
+                            barImage.setImageResource(R.drawable.ic_group_23_image_6);
+                            StyleSetter.setStyles(context, null);
+                        }
+                    }else{
+                        if (bitmap != null) {  //Background mode gradient image half screen
+                            barImage.setImageBitmap(bitmap);
+                            StyleSetter.setStyles(context, bitmap);
+                        } else {
+                            barImage.setImageResource(R.drawable.ic_group_23_image_6);
+                            StyleSetter.setStyles(context, null);
+                        }
+                    }
+                });
+            });
 
         }else{
             barImage.setImageResource(R.drawable.ic_group_23_image_6);
             btnMusicImage.setImageResource(R.drawable.ic_action_note);
             songName.setText("File no longer found");
             artistName.setVisibility(View.GONE);
-            StyleSetter.setInitBackground(context);
-            //StyleSetter.setStyles(context, bitmap);
         }
     }
 
