6bd29e717786595392065464cd56450d3c9f8d02
tiesiogdvd <deivukelis@gmail.com>
2023-01-26 15:16:53 +0200

    Implemented methods for style setting depending on settings background type, added glide for transitioning

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/StyleSetter.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/classes/StyleSetter.java
@@ -5,14 +5,26 @@ import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.TransitionDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.view.View;
 import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.palette.graphics.Palette;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.google.android.material.imageview.ShapeableImageView;
 
 import eu.tutorial.androidapplicationfilesystem.R;
@@ -23,7 +35,8 @@ public class StyleSetter {
 
     static TextView title, artist, remainingText, totalText;
     static NeumorphImageButton btnMusicImage;
-    static ImageView imageView;
+    static ImageView imageView, imageViewGradient;
+    static ConstraintLayout imageViewGradientWrapper;
     static ImageButton btnPlay;
     static ImageButton btnPrev;
     static ImageButton btnNext;
@@ -43,6 +56,7 @@ public class StyleSetter {
         setBackground(context);
     }
 
+
     public static void setStyles(Context context){
         initViews(context);
         setBackground(context);
@@ -52,6 +66,9 @@ public class StyleSetter {
     private static void initViews(Context context){
         btnMusicImage = ((MainActivity)context).findViewById(R.id.musicImage);
         imageView  = ((MainActivity)context).findViewById(R.id.layoutBackground);
+
+        imageViewGradient = ((MainActivity) context).findViewById(R.id.layoutBackgroundGradient);
+        imageViewGradientWrapper = ((MainActivity) context).findViewById(R.id.layoutBackgroundGradientWrapper);
         btnPlay = ((MainActivity)context).findViewById(R.id.playButton);
         btnPrev = ((MainActivity) context).findViewById(R.id.lastButton);
         btnNext = ((MainActivity) context).findViewById(R.id.nextButton);
@@ -68,95 +85,219 @@ public class StyleSetter {
     }
 
     private static void setBackground(Context context){
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inScaled = false;
-        options.inJustDecodeBounds = false;
-
         if(m_bitmap!=null){
-            BitmapDrawable drawable;
-            Bitmap blurredBitmap = BlurBuilder.blur(context,m_bitmap);
-            drawable = new BitmapDrawable(context.getResources(), blurredBitmap);
 
-            setImageDrawableWithAnimation(imageView, drawable, 200);
+            if(!Settings.getBackgroundMode()){
+                Bitmap blurredBitmap = BlurBuilder.blur(context,m_bitmap);
+                Glide.with(context)
+                        .asBitmap()
+                        .load(blurredBitmap)
+                        .skipMemoryCache(true)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .transition(BitmapTransitionOptions.withCrossFade(500))
+                        .centerCrop()
+                        .into(imageView);
 
-            //imageView.setBackground(drawable);
-            //imageView.animate().setStartDelay(100).setDuration(200);
-            setFill(true);
+                imageView.setVisibility(View.VISIBLE);
+
+                imageViewGradientWrapper.setVisibility(View.GONE);
+                btnMusicImage.setVisibility(View.VISIBLE);
+            }else{
+                Glide.with(context)
+                        .asBitmap()
+                        .load(m_bitmap)
+                        .skipMemoryCache(true)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .transition(BitmapTransitionOptions.withCrossFade(500))
+                        .centerCrop()
+                        .into(imageViewGradient);
+
+                imageViewGradientWrapper.setVisibility(View.VISIBLE);
+                imageViewGradient.animate();
+                imageView.setVisibility(View.GONE);
+                btnMusicImage.setVisibility(View.GONE);
+
+            }
+            setFill(true, context);
         }else {
             int UIMode = context.getResources().getConfiguration().uiMode;
-            System.out.println(UIMode);
-
             if(UIMode == 33){ //33 is the night mode
-                m_drawableBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.img_bg_9, options);
-                Bitmap blurredBitmap = BlurBuilder.blur(context,m_drawableBitmap);
-                BitmapDrawable drawable = new BitmapDrawable(context.getResources(), blurredBitmap);
 
-                setImageDrawableWithAnimation(imageView, drawable, 200);
+                if(!Settings.getBackgroundMode()){
+                    imageView.setVisibility(View.VISIBLE);
 
-                //imageView.setBackground(drawable);
-                //imageView.animate().setStartDelay(100).setDuration(200);
-                //imageView.getBackground().setAlpha(40);
-                //imageView.getDrawable().setAlpha(0);
-                //imageView.setImageAlpha(0);
-                //imageView.getDrawable().mutate().setAlpha(0);
+                    imageViewGradientWrapper.setVisibility(View.GONE);
+                    btnMusicImage.setVisibility(View.VISIBLE);
+                    m_drawableBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.img_bg_9);
+                    Bitmap blurredBitmap = BlurBuilder.blur(context,m_drawableBitmap);
+                    Glide.with(context)
+                            .asBitmap()
+                            .load(blurredBitmap)
+                            .skipMemoryCache(true)
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .transition(BitmapTransitionOptions.withCrossFade(500))
+                            .centerCrop()
+                            .into(imageView);
+
+                }else{
+                    imageViewGradientWrapper.setVisibility(View.VISIBLE);
+                    imageViewGradient.animate();
+                    imageView.setVisibility(View.GONE);
+
+                    btnMusicImage.setVisibility(View.GONE);
+                    Drawable image = context.getResources().getDrawable(R.drawable.img_bg_8, context.getTheme());
+                    Glide.with(context)
+                            .asDrawable()
+                            .load(image)
+                            .skipMemoryCache(true)
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .transition(DrawableTransitionOptions.withCrossFade(500))
+                            .centerCrop()
+                            .into(imageViewGradient);
+                }
+
+
 
             }
+
+
+
             if(UIMode == 17){ //17 for the light mode
-                m_drawableBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.img_bg_7, options);
-                Bitmap blurredBitmap = BlurBuilder.blur(context,m_drawableBitmap);
-                BitmapDrawable drawable = new BitmapDrawable(context.getResources(), blurredBitmap);
-                //drawable.setAlpha(0);
+                if(!Settings.getBackgroundMode()){
+                    imageView.setVisibility(View.VISIBLE);
 
-                setImageDrawableWithAnimation(imageView, drawable, 200);
+                    imageViewGradientWrapper.setVisibility(View.GONE);
+                    btnMusicImage.setVisibility(View.VISIBLE);
+                    m_drawableBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.img_bg_7);
+                    Bitmap blurredBitmap = BlurBuilder.blur(context,m_drawableBitmap);
+                    Glide.with(context)
+                            .asBitmap()
+                            .load(blurredBitmap)
+                            .skipMemoryCache(true)
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .transition(BitmapTransitionOptions.withCrossFade(500))
+                            .centerCrop()
+                            .into(imageView);
 
-                //imageView.setBackground(drawable);
-                //imageView.animate().setStartDelay(100).setDuration(200);
-                //imageView.getBackground().setAlpha(20);
-                //imageView.getDrawable().setAlpha(20);
-                //imageView.getDrawable().mutate().setAlpha(0);
-                //imageView.setImageAlpha(0);
+                }else{
+                    imageViewGradientWrapper.setVisibility(View.VISIBLE);
+                    imageViewGradient.animate();
+                    imageView.setVisibility(View.GONE);
+
+                    btnMusicImage.setVisibility(View.GONE);
+                    Drawable image = context.getResources().getDrawable(R.drawable.img_bg_8, context.getTheme());
+                    Glide.with(context)
+                            .asDrawable()
+                            .load(image)
+                            .skipMemoryCache(true)
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .transition(DrawableTransitionOptions.withCrossFade(500))
+                            .centerCrop()
+                            .into(imageViewGradient);
+                }
             }
-            //imageView.setBackground(drawable);
-            //imageView.getBackground().setAlpha(80);
-            setFill(false);
+
+            setFill(false, context);
         }
 
+        if(Settings.getBackgroundMode()){
+            View view = ((MainActivity) context).findViewById(R.id.layoutGradient);
+            int h = view.getHeight();
+            int w = view.getWidth();
+            ((MainActivity) context).getRequestedOrientation();
+            int orientation = context.getResources().getConfiguration().orientation;
+            ShapeDrawable mDrawable = new ShapeDrawable(new RectShape());
+
+
+            if(m_bitmap!=null){
+                Palette palette = Palette.from(m_bitmap).generate();
+                int dominantColor = palette.getDominantColor(Color.WHITE);
+                if(orientation == Configuration.ORIENTATION_PORTRAIT){
+                    mDrawable.getPaint().setShader(new LinearGradient(0,0,0,h,Color.parseColor("#00000000"),dominantColor, Shader.TileMode.CLAMP));
+                }else{
+                    mDrawable.getPaint().setShader(new LinearGradient(0,0,w,0,Color.parseColor("#00000000"),dominantColor, Shader.TileMode.CLAMP));
+                }
+                view.setBackground(mDrawable);
+                //imageView.setBackground(null);
+                imageView.setBackgroundResource(android.R.color.transparent);
+                imageViewGradientWrapper.setVisibility(View.VISIBLE);
+                imageViewGradient.animate();
+                imageView.setVisibility(View.VISIBLE);
+
+                imageView.setBackgroundColor(dominantColor);
+            }else{
+                if(orientation == Configuration.ORIENTATION_PORTRAIT){
+                    mDrawable.getPaint().setShader(new LinearGradient(0,0,0,h,Color.parseColor("#00000000"),context.getColor(R.color.background), Shader.TileMode.CLAMP));
+                }else{
+                    mDrawable.getPaint().setShader(new LinearGradient(0,0,w,0,Color.parseColor("#00000000"),context.getColor(R.color.background), Shader.TileMode.CLAMP));
+                }
+                view.setBackground(mDrawable);
+                imageView.setBackgroundResource(android.R.color.transparent);
+                imageView.setVisibility(View.VISIBLE);
+                //imageView1.setVisibility(View.GONE);
+                imageView.setBackgroundColor(context.getColor(R.color.background));
+            }
+        }
     }
 
 
 
 
-    private static void setFill(Boolean hasImage){
-        int brightness;
-        if(hasImage){
-            brightness = 255-BrightnessCalculator.calculateBrightness(m_bitmap);
-        }else{
-            brightness = 255-BrightnessCalculator.calculateBrightness(m_drawableBitmap);
-        }
-        if(brightness>=123){brightness=255;}
-        if(brightness<=123){brightness=0;}
-        String hex = String.format("#%02X%02X%02X", brightness, brightness, brightness); //3 brightness each stands for R,G,B
-        int color = Color.parseColor(hex);
+    private static void setFill(Boolean hasImage, Context context){
+        if(!Settings.getBackgroundMode()){
+            int brightness;
+            if(hasImage){
+                brightness = 255-BrightnessCalculator.calculateBrightness(m_bitmap);
+            }else{
+                brightness = 255-BrightnessCalculator.calculateBrightness(m_drawableBitmap);
+            }
+            if(brightness>=123){brightness=255;}
+            if(brightness<=123){brightness=0;}
+            String hex = String.format("#%02X%02X%02X", brightness, brightness, brightness); //3 brightness each stands for R,G,B
+            int color = Color.parseColor(hex);
 
-        btnPlay.setColorFilter(color);
-        btnPrev.setColorFilter(color);
-        btnNext.setColorFilter(color);
-        btnPlaylist.setColorFilter(color);
-        btnFav.setColorFilter(color);
-        title.setTextColor(color);
-        artist.setTextColor(color);
-        remainingText.setTextColor(color);
-        totalText.setTextColor(color);
+            btnPlay.setColorFilter(color);
+            btnPrev.setColorFilter(color);
+            btnNext.setColorFilter(color);
+            btnPlaylist.setColorFilter(color);
+            btnFav.setColorFilter(color);
+            title.setTextColor(color);
+            artist.setTextColor(color);
+            remainingText.setTextColor(color);
+            totalText.setTextColor(color);
+        }else{
+            int color = context.getResources().getColor(R.color.text,context.getTheme());
+            btnPlay.setColorFilter(color);
+            btnPrev.setColorFilter(color);
+            btnNext.setColorFilter(color);
+            btnPlaylist.setColorFilter(color);
+            btnFav.setColorFilter(color);
+            title.setTextColor(color);
+            artist.setTextColor(color);
+            remainingText.setTextColor(color);
+            totalText.setTextColor(color);
+
+
+            //btnPlay.getBackground().setTint(context.getResources().getColor(R.color.background, context.getTheme()));
+
+        }
     }
 
     public static void setInitBackground(Context context){
         ImageView imageView = ((MainActivity)context).findViewById(R.id.layoutBackground);
-        Bitmap drawableBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.img_bg_8);
-        m_drawableBitmap = drawableBitmap;
-        Bitmap blurredBitmap = BlurBuilder.blur(context,drawableBitmap);
-        BitmapDrawable drawable = new BitmapDrawable(context.getResources(), blurredBitmap);
-        setImageDrawableWithAnimation(imageView, drawable, 200);
-        imageView.getBackground().setAlpha(0);
+        Bitmap drawableBitmap;
+        if(!Settings.getBackgroundMode()){
+            drawableBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.img_bg_8);
+            m_drawableBitmap = drawableBitmap;
+            Bitmap blurredBitmap = BlurBuilder.blur(context,drawableBitmap);
+            BitmapDrawable drawable = new BitmapDrawable(context.getResources(), blurredBitmap);
+            setImageDrawableWithAnimation(imageView, drawable, 200);
+            imageView.getBackground().setAlpha(0);
+        }else{
+            int color = context.getResources().getColor(R.color.text,context.getTheme());
+            imageView.setBackgroundColor(color);
+            imageView.getBackground().setAlpha(255);
+        }
     }
 
     public static void setImageDrawableWithAnimation(ImageView imageView, Drawable drawable, int duration) {
@@ -169,5 +310,7 @@ public class StyleSetter {
         transitionDrawable.setCrossFadeEnabled(true);
         imageView.setImageDrawable(transitionDrawable);
         transitionDrawable.startTransition(duration);
+        imageView.setScaleType(ImageView.ScaleType.FIT_END);
     }
+
 }
