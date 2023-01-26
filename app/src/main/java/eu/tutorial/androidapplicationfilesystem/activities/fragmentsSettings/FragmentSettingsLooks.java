045d258c91f0d6a3d8508bb64af2acf6abb1dfc1
tiesiogdvd <deivukelis@gmail.com>
2023-01-26 15:22:02 +0200

    Added setting to change background mode

--- app/src/main/java/eu/tutorial/androidapplicationfilesystem/activities/fragmentsSettings/FragmentSettingsLooks.java
+++ app/src/main/java/eu/tutorial/androidapplicationfilesystem/activities/fragmentsSettings/FragmentSettingsLooks.java
@@ -1,60 +1,32 @@
 package eu.tutorial.androidapplicationfilesystem.activities.fragmentsSettings;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import eu.tutorial.androidapplicationfilesystem.R;
+import eu.tutorial.androidapplicationfilesystem.classes.Settings;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentSettingsLooks#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentSettingsLooks extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentSettingsLooks() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentSettingsLooks.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentSettingsLooks newInstance(String param1, String param2) {
-        FragmentSettingsLooks fragment = new FragmentSettingsLooks();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    SharedPreferences sh;
+    SwitchMaterial imageCovering;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -63,4 +35,45 @@ public class FragmentSettingsLooks extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings_looks, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initViews(view);
+        initSettings();
+        initBarListeners();
+        initListeners();
+    }
+
+
+
+    private void initViews(View view) {
+        sh = requireActivity().getSharedPreferences("lastMusic",MODE_PRIVATE);
+        Settings.setSettings(sh);
+        imageCovering = view.findViewById(R.id.settingsLooksImageCovering);
+    }
+
+    private void initSettings() {
+        imageCovering.setChecked(Settings.getBackgroundMode());
+    }
+
+    private void initListeners() {
+        imageCovering.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Settings.setBackgroundMode(isChecked);
+            }
+        });
+    }
+
+    private void initBarListeners() {
+
+    }
+
+
+
+
+
+
 }
